9d37a58dd6f602127f62f4515df45b825e3c8785
brusca359 <162684965+brusca359@users.noreply.github.com>
2024-04-07 18:56:30 +0200

    Update hand.js

--- hand.js
+++ hand.js
@@ -1,14 +1,22 @@
 //import Deck from "./deck";
 export default class Hand{
-    constructor(mazzo){
-        this.carte=manoIniziale(mazzo);
+    constructor(deck){
+        this.carte=manoIniziale(deck);
     }
 }
 
 //Finzioni mano
-function manoIniziale(mazzo){
+/*function manoIniziale(mazzo){
     var ret= new Array();
     ret=mazzo.slice(0,2); //prime 3 carte di mazzo in ret
     mazzo=mazzo.slice(3,); //togli le carte dal mazzo
+    return ret;
+}*/
+
+function manoIniziale(deck){//in tipo 1.3hr non so perche non funzionava al inizio che sto facendo la stessa cosa??? adesso però ho molto piu familiare la struttura quindi poggers?
+    var ret= new Array();
+    for(let i=0; i<3; i++){
+        ret.push(deck.carte.pop());
+    }
     return ret;
 }

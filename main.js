1c57401599a725f27219ce2f8692258c7d5bd2ed
brusca359 <162684965+brusca359@users.noreply.github.com>
2024-04-07 18:57:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import Deck from "./deck.js";
 import Hand from "./hand.js";
 
+
 const mazzoGiocatore = new Deck();
 mazzoGiocatore.mescola();
 const manoGiocatore = new Hand(mazzoGiocatore);
 
 
-
-console.log(mazzoUno.carte);
-console.log(mano.carte);
-console.log(mazzoUno.carte);
+console.log("questa è mazzo prima di pescare")//se li metto nella stessa riga non mi fa vedere specifiche di carte ma li vede solo come object
+console.log(mazzoGiocatore.carte);
+console.log("questa è la mano")
+console.log(manoGiocatore.carte);
+console.log("questa è mazzo dopo")
+console.log(mazzoGiocatore.carte);

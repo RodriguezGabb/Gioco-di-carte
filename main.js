59feb88bd35e67804eb4ec731fa374d0a3d2cb31
brusca359 <162684965+brusca359@users.noreply.github.com>
2024-05-01 20:33:57 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
 import Deck from "./deck.js";
 import Hand from "./hand.js";
 
-const manaCattivo=(function(){//cosi dovrebbe essere globale ma modificabile solo dalle mie funzioni
-  let value=5;
-  function getValue(){
-      return value;
-    }
-  function updManaCattivo(i){
-    if(!isNaN(i)){//se i è un numero
-      manaCattivo+=i;
+const manaCattivo = (function () {//cosi dovrebbe essere globale ma modificabile solo dalle mie funzioni
+  let value = 5;
+  function getValue() {
+    return value;
+  }
+  function updManaCattivo(i) {
+    if (!isNaN(i)) {//se i è un numero
+      manaCattivo += i;
     }
     else throw new exception("updManaCattivo ha ricevuto NaN")
   }
-  function resetManaCattivo(){//funzione per il fine turno 
-   manaCattivo=5;
+  function resetManaCattivo() {//funzione per il fine turno 
+    manaCattivo = 5;
   }
 })();
 
-const manaBuono=(function(){//cosi dovrebbe essere globale ma modificabile solo dalle mie funzioni
-  let value=5;
-  function getValue(){
-      return value;
-    }
-  function updManaBuono(i){
-    if(!isNaN(i)){//se i è un numero
-      manaBuono+=i;
+const manaBuono = (function () {//cosi dovrebbe essere globale ma modificabile solo dalle mie funzioni
+  let value = 5;
+  function getValue() {
+    return value;
+  }
+  function updManaBuono(i) {
+    if (!isNaN(i)) {//se i è un numero
+      manaBuono += i;
       updManaBar(i);
     }
     else throw new exception("updManaBuono ha ricevuto NaN")
   }
-  function resetManaBuono(){//funzione per il fine turno 
-   manaBuono=5;
-   resetManaBar();
+  function resetManaBuono() {//funzione per il fine turno 
+    manaBuono = 5;
+    resetManaBar();
   }
 })();
 
@@ -48,34 +48,34 @@ console.log(mazzoGiocatore.carte);
 
 //Rivedere i vari getelementbyid perché si possono fare delle variabbili globali
 
-document.getElementById("mazzoGiocatore").addEventListener("click",giocatorePesca);
+document.getElementById("mazzoGiocatore").addEventListener("click", giocatorePesca);
 
 //potremmo sistemare questo codice spostando tutte le funzioni con carte nel file giusto(hand o deck)!!!!!!!!!!!!!!!!!!!!!
 
 
 // Funzione che fa comparire il pulsante dentro le carte
-function carteOnClick(id1){
-        console.log("si? dica")
-        var button = document.getElementById(id1);
-        if (button.style.display === "none"){//Se il bottone è non è visibile lo rende visibile altrimenti il contrario 
-          button.style.display = "block";
-        } else {
-          button.style.display = "none";
-        }
+function carteOnClick(id1) {
+  console.log("si? dica")
+  var button = document.getElementById(id1);
+  if (button.style.display === "none") {//Se il bottone è non è visibile lo rende visibile altrimenti il contrario 
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
 }
 // Funzione che fa giocare la carta
 function giocaCarta(id1) {
-    var div1 = document.getElementById(id1);//prende la carta con l'id e lo mette sulla board
-    var div2 = document.getElementById("boardGiocatore");//Vorrei aggiungere un limite sulla board ma ne dobbiamo parlare  
-    div2.appendChild(div1);
+  var div1 = document.getElementById(id1);//prende la carta con l'id e lo mette sulla board
+  var div2 = document.getElementById("boardGiocatore");//Vorrei aggiungere un limite sulla board ma ne dobbiamo parlare  
+  div2.appendChild(div1);
 }
 //funzione per far pescare il giocatore
-function giocatorePesca(){
+function giocatorePesca() {
   var manoGiocatore = document.getElementById("manoGiocatore");
-  var nCarteInManoGiocatore=manoGiocatore.querySelectorAll("div").length;
-  var nCarteGiocatore=document.getElementsByClassName("carteManoGiocatore").length
-  if(nCarteInManoGiocatore<5){//5 num max di carte in mano
-    var nuovaCarta=creaCarta(nCarteGiocatore+1);
+  var nCarteInManoGiocatore = manoGiocatore.querySelectorAll("div").length;
+  var nCarteGiocatore = document.getElementsByClassName("carteManoGiocatore").length
+  if (nCarteInManoGiocatore < 5) {//5 num max di carte in mano
+    var nuovaCarta = creaCarta(nCarteGiocatore + 1);
     manoGiocatore.appendChild(nuovaCarta);
   }//else
 }
@@ -83,46 +83,46 @@ function giocatorePesca(){
 //Funzione di creazione della prima carta del mazzo
 //Questa funzione deve ancora essere realizata (vedi con andre come e cosa vogliamo fa)
 //i è il numero della carta (alla fine servirà solo per l'id)
-function creaCarta(i){
-  var carta=document.createElement("div");
-  carta.className="carteManoGiocatore";
-  carta.setAttribute("id", "carteManoGiocatore"+i);
-  carta.innerHTML="Card "+i//cosa per debug
-  var bottone=document.createElement("button");
-  bottone.className="bottoneGioca";
-  bottone.setAttribute("id","bottoneGioca"+i);console.log("bottoneGioca"+i);
-  bottone.innerHTML="Gioca";
+function creaCarta(i) {
+  var carta = document.createElement("div");
+  carta.className = "carteManoGiocatore";
+  carta.setAttribute("id", "carteManoGiocatore" + i);
+  carta.innerHTML = "Card " + i//cosa per debug
+  var bottone = document.createElement("button");
+  bottone.className = "bottoneGioca";
+  bottone.setAttribute("id", "bottoneGioca" + i); console.log("bottoneGioca" + i);
+  bottone.innerHTML = "Gioca";
   carta.appendChild(bottone);
-  bottone.style.display="none";
-  carta.addEventListener("click",function (){
+  bottone.style.display = "none";
+  carta.addEventListener("click", function () {
     carteOnClick(bottone.id);
   });
 
-  bottone.addEventListener("click",function (){
+  bottone.addEventListener("click", function () {
     giocaCarta(carta.id);
   });
   return carta;
-} 
+}
 
-function avversarioPesca(){
+function avversarioPesca() {
   var manoAvversario = document.getElementById("manoAvversario");
-  var nCarteInManoAvversario=manoAvversario.querySelectorAll("div").length;
-  var nCarteAvversario=document.getElementsByClassName("manoAvversario").length
-  if(nCarteInManoAvversario<5){//5 num max di carte in mano
-    var nuovaCarta=creaCartaAvv(nCarteAvversario+1);
+  var nCarteInManoAvversario = manoAvversario.querySelectorAll("div").length;
+  var nCarteAvversario = document.getElementsByClassName("manoAvversario").length
+  if (nCarteInManoAvversario < 5) {//5 num max di carte in mano
+    var nuovaCarta = creaCartaAvv(nCarteAvversario + 1);
     manoAvversario.appendChild(nuovaCarta);
   }
 }
 
 /*test per vedere se ho capito il tuo codice*/
-function creaCartaAvv(i){
-  var carta=document.createElement("div");
-  carta.className="carteManoAvv";
-  carta.setAttribute("id", "carteManoAvv"+i);
-  carta.innerHTML="Card "+i//cosa per debug
+function creaCartaAvv(i) {
+  var carta = document.createElement("div");
+  carta.className = "carteManoAvv";
+  carta.setAttribute("id", "carteManoAvv" + i);
+  carta.innerHTML = "Card " + i//cosa per debug
   return carta;
-} 
-function giocaCartaAvv(carta){
+}
+function giocaCartaAvv(carta) {
   giocaCarta(carta.id);
 }
 
@@ -133,53 +133,46 @@ const nCimA = document.getElementById('nCardCimitero');
 
 
 function updateNumber(number) {
-    nCimA.textContent = number;
+  nCimA.textContent = number;
 }
 
 
 updateNumber(5); // se esce 68 non è partita
 
 //ia section
-function mydumbPlay(hand){
-  var costi=[];
-  for(let i=0;i<hand.length;i++){//creo lista di costi
-    costi[i]=hand[i].costo;
+function mydumbPlay(hand) {
+  var costi = [];
+  for (let i = 0; i < hand.length; i++) {//creo lista di costi
+    costi[i] = hand[i].costo;
   }
   const nCarte = costi.length;
-  console.log(nCarte);
-  var arrayFinale=[nCarte+1];//se carta usata ha nel suo slot true senno false
-  console.log(arrayFinale);
-  
-  arrayFinale[0]=999;//in primo slot metto spesa massima della combinazione
+  var arrayFinale = [nCarte + 1];//se carta usata ha nel suo slot true senno false
+
+  arrayFinale[0] = 999;//in primo slot metto spesa massima della combinazione
 
 
-  var arrayTemp=[nCarte];//ci salvo bool delle varie carte in combinazione
-  var costoMax=manaCattivo.getManaCattivo();//costo della combinazione attuale
-  for(let cartaIni = 0; cartaIni <= nCarte-1; cartaIni++){
-
-    
-    for(let pos=0;pos<= nCarte-1; pos++){
-      var ciclo=(pos+cartaIni)%nCarte;
-      console.log("ciclo"+ciclo);
-      if(costi[ciclo]<=costoMax){
-        costoMax=costoMax-costi[ciclo];
-        arrayTemp[ciclo]=true;
+  var arrayTemp = [nCarte];//ci salvo bool delle varie carte in combinazione
+  var costoMax = manaCattivo.getManaCattivo();//costo della combinazione attuale
+  for (let cartaIni = 0; cartaIni <= nCarte - 1; cartaIni++) {
+    for (let pos = 0; pos <= nCarte - 1; pos++) {
+      var ciclo = (pos + cartaIni) % nCarte;//ti cicla per tutte le carte ma inizi da un altro punto es 2,3,0,1
+      if (costi[ciclo] <= costoMax) {//se costa più di quanto hai non la puoi lanciare
+        costoMax = costoMax - costi[ciclo];
+        arrayTemp[ciclo] = true;//aggiorni l array e il costo rimanente
       }
     }
-    console.log("-----");
-    
-    if(costoMax<arrayFinale[0]){
-      arrayFinale[0]=costoMax;
-      for(let i=0;i<nCarte;i++){//se trovo combinazione migliore aggiorno arrayFinale
-        arrayFinale[i+1]=arrayTemp[i];
+    if (costoMax < arrayFinale[0]) {//se ne trovo una migliore aggiorno l arrayFinale
+      arrayFinale[0] = costoMax;
+      for (let i = 0; i < nCarte; i++) {
+        arrayFinale[i + 1] = arrayTemp[i];
       }
-    }  
-    costoMax=manaCattivo.getManaCattivo()//quando vado a carta dopo resetto tutto
-    for(let i=0;i<nCarte;i++){//resetto anche l array
-      arrayTemp[i]=false;
+    }
+    costoMax = manaCattivo.getManaCattivo()//quando vado a carta dopo resetto tutto
+    for (let i = 0; i < nCarte; i++) {//resetto anche l array
+      arrayTemp[i] = false;
     }
   }
   return arrayFinale;
-  }
+}
 
 

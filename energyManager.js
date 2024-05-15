39696d11352e404cb52e94c34bea00fa9a8f9764
brusca359 <162684965+brusca359@users.noreply.github.com>
2024-05-16 01:26:55 +0200

    Add files via upload

--- energyManager.js
+++ energyManager.js
@@ -1,7 +1,8 @@
 // Define fullness globally
 window.GoodManafullness = 0;
 window.EvilManafullness = 0;
-
+const evilManaNumber = document.getElementById("eviManaNumber");
+const goodManaNumber = document.getElementById("goodManaNumber");
 // Function to update the bar based on the variable value
 function setGoodManaBar() {
     const fillGoodMana = document.getElementById('fillGoodMana');
@@ -18,20 +19,30 @@ function updGoodManaBar(i) {
     if (Number.isNaN(i)) { throw new Error("energia Good variata con NaN") }
     window.GoodManafullness += i;
     setGoodManaBar();
+    updateGoodManaNumber(window.GoodManafullness);
 }
 function updEvilManaBar(i) {
     if (Number.isNaN(i)) { throw new Error("energia Evil variata con NaN") }
     window.EvilManafullness += i;
     setEvilManaBar();
+    updateEvilManaNumber(window.EvilManafullness);
 }
 function resetGoodManaBar() {
     window.GoodManafullness = 5;
     setGoodManaBar();
+    updateGoodManaNumber(window.GoodManafullness);
 }
 function resetEvilManaBar() {
     window.EvilManafullness = 5;
     setEvilManaBar();
+    updateEvilManaNumber(window.EvilManafullness);
 }
 
+function updateEvilManaNumber(number) {
+    evilManaNumber.textContent = number; // Update the text content of the number div
+}
+function updateGoodManaNumber(number) {
+    goodManaNumber.textContent = number; // Update the text content of the number div
+}
 resetGoodManaBar();
 resetEvilManaBar();
--- lifeManager.js
+++ lifeManager.js
@@ -3,6 +3,10 @@ window.GoodLifeFullness = 0;
 window.EvilLifeFullness = 0;
 window.GoodarmourFullness = 0;
 window.EvilarmourFullness = 0;
+const evilLifeNumber = document.getElementById("evilLifeNumber");
+const goodLifeNumber = document.getElementById("goodLifeNumber");
+const evilArmourNumber = document.getElementById("evilArmourNumber");
+const goodArmourNumber = document.getElementById("goodArmourNumber");
 
 // Function to update the bar based on the variable value
 function setGoodLifeBar() {
@@ -32,16 +36,19 @@ function updGoodArmour(i) {
         if (i <= window.GoodarmourFullness) {//danno<=armatura
             window.GoodarmourFullness = window.GoodarmourFullness + i;
             setGoodarmourBar();
+            updateGoodArmourNumber(window.GoodarmourFullness);
             return 0;
         }
         let temp = i - window.GoodarmourFullness;
         window.GoodarmourFullness = 0;
         setGoodarmourBar();
+        updateGoodArmourNumber(window.GoodarmourFullness);
         return temp;//cosi sappiamo danno extra da infliggere
     }
     //se armatura
     window.GoodarmourFullness += i;
     setGoodarmourBar();
+    updateGoodArmourNumber(window.GoodarmourFullness);
     return 0;
 }
 function updEvilArmour(i) {
@@ -50,49 +57,76 @@ function updEvilArmour(i) {
         if (i <= window.EvilarmourFullness) {//danno<=armatura
             window.EvilarmourFullness = window.EvilarmourFullness + i;
             setEvilarmourBar();
+            updateEvilArmourNumber(window.EvilarmourFullness);
             return 0;
         }
         let temp = i - window.EvilarmourFullness;
         window.EvilarmourFullness = 0;
         setEvilarmourBar();
+        updateEvilArmourNumber(window.EvilarmourFullness);
         return temp;//cosi sappiamo danno extra da infliggere
     }
     //se è più armatura
     window.EvilarmourFullness += i;
     setEvilarmourBar();
+    updateEvilArmourNumber(window.EvilarmourFullness);
     return 0;
 }
 function resetGoodArmour() {
     window.GoodarmourFullness = 0;
     setGoodarmourBar();
+    updateGoodArmourNumber(0);
 }
 function resetEvilArmour() {
     window.EvilarmourFullness = 0;
     setEvilarmourBar();
+    updateEvilArmourNumber(0);
 }
 function updGoodLifeBar(i) {
     if (Number.isNaN(i)) { throw new Error("life variata con NaN") }
-    if (i < 0 && i > window.GoodLifeFullness) {
-        alert("hai perso")
+    if (i <= 0 && i >= window.GoodLifeFullness) {
+        window.GoodLifeFullness += i;
+        setGoodLifeBar();
+        updateGoodLifeNumber(window.GoodLifeFullness);
+        alert("hai perso");
     }
     window.GoodLifeFullness += i;
     setGoodLifeBar();
+    updateGoodLifeNumber(window.GoodLifeFullness);
 }
 function updEvilLifeBar(i) {
     if (Number.isNaN(i)) { throw new Error("life variata con NaN") }
-    if (i < 0 && i > window.EvilLifeFullness) {
-        alert("hai vinto")
+    if (i <= 0 && i >= window.EvilLifeFullness) {
+        window.EvilLifeFullness += i;
+        setEvilLifeBar();
+        updateEvilLifeNumber(window.EvilLifeFullness);
+        alert("hai vinto");
     }
     window.EvilLifeFullness += i;
     setEvilLifeBar();
+    updateEvilLifeNumber(window.EvilLifeFullness);
 }
 function resetGoodLifeBar() {
     window.GoodLifeFullness = 30;
     setGoodLifeBar();
+    updateGoodLifeNumber(30);
 }
 function resetEvilLifeBar() {
     window.EvilLifeFullness = 30;
     setEvilLifeBar();
+    updateEvilLifeNumber(30);
+}
+function updateEvilLifeNumber(number) {
+    evilLifeNumber.textContent = number; // Update the text content of the number div
+}
+function updateGoodLifeNumber(number) {
+    goodLifeNumber.textContent = number; // Update the text content of the number div
+}
+function updateEvilArmourNumber(number) {
+    evilArmourNumber.textContent = number; // Update the text content of the number div
+}
+function updateGoodArmourNumber(number) {
+    goodArmourNumber.textContent = number; // Update the text content of the number div
 }
 resetGoodLifeBar();
 resetEvilLifeBar();

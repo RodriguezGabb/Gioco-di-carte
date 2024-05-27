ee7f8b3a3dbefb074504924600137401cb5a3171
brusca359 <162684965+brusca359@users.noreply.github.com>
2024-05-28 01:08:38 +0200

    Add files via upload

--- server/display_data.php
+++ server/display_data.php
@@ -9,15 +9,58 @@ require 'db_connection.php';
 <head>
     <meta charset="UTF-8">
     <title>Visualizza Dati</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f9;
+            color: #333;
+            margin: 0;
+            padding: 20px;
+        }
+
+        h2 {/*modifica titolo*/
+            color: #555;
+            text-align: center;
+            border: 1px solid black;
+            background-color: darkgrey;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        tr:hover {
+            background-color: #f1f1f1;
+        }
+    </style>
 </head>
 
 <body>
-    <h2>Dati della Tabella User</h2>
+    <h2>Classifica</h2>
     <?php
     $query = 'SELECT * FROM classifica ORDER BY "turn" ASC';
     $result = pg_query($query);
-    // Printing results in HTML
     echo "<table>\n";
+    echo "<tr><th>Username</th><th>Punteggio</th></tr>\n"; 
     while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
         echo "\t<tr>\n";
         foreach ($line as $col_value) {
